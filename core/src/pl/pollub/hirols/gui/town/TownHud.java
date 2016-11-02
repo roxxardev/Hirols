41ee80072eb2daffa35e2696095f0235ca69f962
Eryk Puławski <eryk.pulawski@pollub.edu.pl>
2016-11-02 23:53:01 +0100

    units ui in Town

--- core/src/pl/pollub/hirols/gui/town/TownHud.java
+++ core/src/pl/pollub/hirols/gui/town/TownHud.java
@@ -1,14 +1,9 @@
 package pl.pollub.hirols.gui.town;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.Scaling;
 import com.kotcrab.vis.ui.layout.GridGroup;
 import com.kotcrab.vis.ui.widget.VisImageButton;
@@ -25,11 +20,9 @@ import pl.pollub.hirols.gui.TopBar;
 public class TownHud extends Hud {
     private TopBar topBar;
 
-    private VisImageButton exitButton;
-    private GridGroup inTown, outTown;
+    private Garrison inTown, outTown;
 
-    private Table inTownTable, outTownTable;
-    private VisImageTextButton heroInTown, heroOutOfTown;
+    private VisImageButton exitButton;
 
     public TownHud(Hirols game) {
         super(game);
@@ -44,55 +37,24 @@ public class TownHud extends Hud {
         buttonGroup.setMaxCheckCount(1);
         buttonGroup.setMinCheckCount(0);
         buttonGroup.setUncheckLast(false);
-        inTown = new GridGroup(40f,2);
-        outTown = new GridGroup(40f,2);
-
-        for(int i = 0; i < 5; i++) {
-            VisImageTextButton in = new VisImageTextButton("Dupa", game.hudManager.skin.get("units-style", VisImageTextButton.VisImageTextButtonStyle.class));
-            inTown.addActor(in);
-            buttonGroup.add(in);
-            in.getImage().setScaling(Scaling.stretch);
-            in.clearChildren();
-            in.add(in.getImage()).expand().fill().row();
-            in.add(in.getLabel());
-
-            VisImageTextButton out = new VisImageTextButton("Dupa", game.hudManager.skin.get("units-style", VisImageTextButton.VisImageTextButtonStyle.class));
-            out.getImage().setScaling(Scaling.stretch);
-            out.clearChildren();
-            out.add(out.getImage()).expand().fill().row();
-            out.add(out.getLabel());
-            outTown.addActor(out);
-            buttonGroup.add(out);
-        }
-
-        heroInTown = new VisImageTextButton("Hero", game.hudManager.skin.get("units-style", VisImageTextButton.VisImageTextButtonStyle.class));
-        heroInTown.getImage().setScaling(Scaling.stretch);
-        heroInTown.clearChildren();
-        heroInTown.add(heroInTown.getImage()).expand().fill().row();
-        heroInTown.add(heroInTown.getLabel());
-        buttonGroup.add(heroInTown);
-
-        inTownTable = new Table();
-        inTownTable.addActor(inTown);
-        inTownTable.addActor(heroInTown);
-
-        heroOutOfTown = new VisImageTextButton("Hero", game.hudManager.skin.get("units-style", VisImageTextButton.VisImageTextButtonStyle.class));
-        heroOutOfTown.getImage().setScaling(Scaling.stretch);
-        heroOutOfTown.clearChildren();
-        heroOutOfTown.add(heroOutOfTown.getImage()).expand().fill().row();
-        heroOutOfTown.add(heroOutOfTown.getLabel());
-        buttonGroup.add(heroOutOfTown);
-
-        outTownTable = new Table();
-        outTownTable.addActor(outTown);
-        outTownTable.addActor(heroOutOfTown);
 
         Image townBackground = new Image(game.assetManager.get("towns/snow-town.png", Texture.class));
         townBackground.setFillParent(true);
+
+        inTown = new Garrison(buttonGroup,false);
+        outTown = new Garrison(buttonGroup, true);
+
         stage.addActor(townBackground);
         stage.addActor(topBar);
-        stage.addActor(inTownTable);
-        stage.addActor(outTownTable);
+        stage.addActor(inTown);
+        stage.addActor(outTown);
+    }
+
+    private void moveTextLabelBelowImage(VisImageTextButton button) {
+        button.getImage().setScaling(Scaling.stretch);
+        button.clearChildren();
+        button.add(button.getImage()).expand().fill().row();
+        button.add(button.getLabel());
     }
 
     @Override
@@ -100,16 +62,57 @@ public class TownHud extends Hud {
         super.resize(width, height);
         topBar.resize(width,height);
 
+        inTown.resize(width,height);
+        outTown.resize(width,height);
+    }
 
-        inTown.setSize(topBar.getHeight()*10 + 2, topBar.getHeight()*2);
-        inTown.setItemSize((int)topBar.getHeight()*2 - 2);
-        inTownTable.setBounds(topBar.getHeight(),topBar.getHeight(),inTown.getWidth(), inTown.getHeight()*2);
-        heroInTown.setBounds(inTownTable.getWidth(), 0, inTown.getItemWidth()*2, inTown.getHeight()*2);
+    private class Garrison extends Table {
+        private GridGroup units = new GridGroup();
+        private VisImageTextButton heroButton;
+        private ButtonGroup buttonGroup;
+        private boolean heroOnLeft;
 
-        outTown.setSize(topBar.getHeight()*10 + 2, topBar.getHeight()*2);
-        outTown.setItemSize((int)topBar.getHeight()*2 - 2);
-        outTownTable.setBounds(stage.getWidth() - topBar.getHeight()*11, topBar.getHeight(), inTownTable.getWidth(), inTownTable.getHeight());
-        heroOutOfTown.setBounds(-outTown.getItemWidth()*2, 0, outTown.getItemWidth()*2, outTown.getHeight()*2);
+        public Garrison(ButtonGroup<VisImageTextButton> buttonGroup, boolean heroOnLeft) {
+            this.buttonGroup = buttonGroup;
+            this.heroOnLeft = heroOnLeft;
+
+            units.setSpacing(2);
+
+            for(int i = 0; i < 5; i++) {
+                VisImageTextButton in = new VisImageTextButton("Dupa", game.hudManager.skin.get("units-style", VisImageTextButton.VisImageTextButtonStyle.class));
+                units.addActor(in);
+                buttonGroup.add(in);
+                moveTextLabelBelowImage(in);
+            }
+
+            heroButton = new VisImageTextButton("Hero", game.hudManager.skin.get("units-style", VisImageTextButton.VisImageTextButtonStyle.class));
+            moveTextLabelBelowImage(heroButton);
+            buttonGroup.add(heroButton);
+
+            addActor(units);
+            addActor(heroButton);
+        }
+
+        public void resize(float width, float height) {
+            int divider = 20;
+            int buttonSize = (int) (width / divider);
+            float spacing = units.getSpacing();
+
+            units.setItemSize(buttonSize);
+            units.setSize((buttonSize+spacing)*5+spacing , buttonSize + spacing*2);
+
+            if(!heroOnLeft) {
+                setSize(units.getWidth(), units.getHeight()*2);
+                setPosition(buttonSize,buttonSize);
+                heroButton.setBounds(units.getWidth() + spacing, 0 + spacing, buttonSize*2, buttonSize*2);
+            } else {
+                setSize(units.getWidth(), units.getHeight()*2);
+                setPosition(stage.getWidth() - getWidth() - buttonSize, buttonSize);
+                heroButton.setSize(buttonSize*2,buttonSize*2);
+                heroButton.setPosition(-heroButton.getWidth() - spacing, 0 + spacing);
+            }
+        }
+
     }
 }
 
